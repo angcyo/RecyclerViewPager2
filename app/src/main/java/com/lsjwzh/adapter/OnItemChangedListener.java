b2496fc8424226790e15795542fa7d31e7a4db96
angcyo <angcyo@126.com>
2017-01-16 03:32:42 +0800

    2017-01-16

--- app/src/main/java/com/lsjwzh/adapter/OnItemChangedListener.java
+++ app/src/main/java/com/lsjwzh/adapter/OnItemChangedListener.java
@@ -2,7 +2,7 @@ package com.lsjwzh.adapter;
 
 public interface OnItemChangedListener {
 
-  boolean onItemMove(int fromPosition, int toPosition);
+    boolean onItemMove(int fromPosition, int toPosition);
 
-  void onItemDismiss(int position);
+    void onItemDismiss(int position);
 }
